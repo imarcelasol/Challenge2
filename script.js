0e44911a51694d0769f429f5afd11afa725e7f86
imarcelasol <105014941+imarcelasol@users.noreply.github.com>
2023-07-17 18:21:34 -0300

    subo challenge2

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-var screen = document.querySelector('#screen'); 
+var screen = document.querySelector('#screen');
+var btn = document.querySelectorAll('.btn');
 
-var btn = document.querySelectorAll('.btn'); 
-
-//Obtiene el valor del botón, usando un bucle for 
 for (item of btn) {
     item.addEventListener('click', (e) => {
-        btntext = e.target.innerText; 
+        btntext = e.target.innerText;
 
-        // Si el texto del botón es '×', lo reemplaza con '*'
-        if (btntext == '×') {
+        if (btntext == 'Ã—') {
             btntext = '*';
         }
 
-        // Si el texto del botón es '÷', lo reemplaza con '/'
-        if (btntext == '÷') {
+        if (btntext == 'Ã·') {
             btntext = '/';
         }
-
-        screen.value += btntext; 
+        screen.value += btntext;
     });
 }
 
-// Calcula el valor en pantalla elevado al cuadrado
+
+
 function pow() {
-    screen.value = Math.pow(screen.value, 2); 
+    screen.value = Math.pow(screen.value, 2);
 }
-// Calcula la raíz cuadrada del valor en pantalla
+
 function sqrt() {
-    screen.value = Math.sqrt(screen.value, 2); 
+    screen.value = Math.sqrt(screen.value, 2);
 }
 
 function log() {
-    screen.value = Math.log(screen.value); // Calcula el logaritmo natural del valor en pantalla
+    screen.value = Math.log(screen.value);
 }
 
 
@@ -39,17 +35,15 @@ function fact() {
     var i, num, f;
     f = 1
     num = screen.value;
-
-    // Calcula el factorial del valor en pantalla usando un bucle for
     for (i = 1; i <= num; i++) {
         f = f * i;
     }
 
     i = i - 1;
 
-    screen.value = f; 
+    screen.value = f;
 }
-// Borra el último carácter en la pantalla
+
 function backspc() {
-    screen.value = screen.value.substr(0, screen.value.length - 1); 
+    screen.value = screen.value.substr(0, screen.value.length - 1);
 }
